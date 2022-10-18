d7cdca465b0a5b72a873545149aa2bcc8b9b98d6
Ingrid <65837228+netuno111@users.noreply.github.com>
2022-10-18 19:31:52 -0300

    loja com front end

---
+++ index.js
@@ -0,0 +1,7 @@
+const button1 = document.getElementsByTagName('button');
+const button2 = document.getElementsByTagName('button');
+
+
+function abrirMensagem() {
+    alert('Site em construção, logo terá novidades!');
+}
